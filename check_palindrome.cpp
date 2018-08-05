71680399eb725b6378b87ca69a0e2a166e67ca52
hung.do <hung.do@dominionenterprises.com>
2018-08-05 10:50:01 -0400

    clean up code for challenge 2

--- check_palindrome.cpp
+++ check_palindrome.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
+#include <math.h>       /* pow */
 using namespace std;
 
 bool check_palindrome(long n);
 
 int main()
 {
-    long number = 99999;
-    long maxj=11111;
+    int num;
+    cout << "Enter number of digits: ";
+    cin >> num;
 
-    long max = 0;
+    long first_number = pow(10,num) - 1;
+    long second_number = first_number/9;
+
+    long maxj=second_number;
+
+    long largest_palindrome = 0;
     long i,j;
 
-    for (i=99999; i>11111;i--)
+    for (i=first_number; i>second_number;i--)
         for (j=i;j>maxj;j--) {
             if (check_palindrome(i*j)) {
-                if (i*j > max) {
+                if (i*j > largest_palindrome) {
                     maxj=j;
-                    max = i*j;
-                    cout << max << " : is the current max palindrome with ";
+                    largest_palindrome = i*j;
+                    cout << largest_palindrome << " : is the current max palindrome with ";
                     cout << "(i,j) are " << i << " and " << j << endl;
                 }
             }
