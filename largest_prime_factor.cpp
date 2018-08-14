8de223307e29fbe95ba27fd1924154c82e479643
hung.do <hung.do@dominionenterprises.com>
2018-08-14 07:09:33 -0400

    minor code factor

--- largest_prime_factor.cpp
+++ largest_prime_factor.cpp
@@ -5,8 +5,8 @@ int main()
 {
 	long n = 778118621513;
 	
-	for (int i = 2; i <= sqrt(n); i += 1) {
-    	while (n % i == 0) {
+	for (int i = 3; i <= sqrt(n); i += 2) {
+		while (n % i == 0) {
     		n = n / i;
     	}
 	}
