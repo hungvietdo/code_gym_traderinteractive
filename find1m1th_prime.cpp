cd116dcdf938eb4da263e9e52b8176a4c0208112
Hung Do <hung.do@traderinteractive.com>
2018-09-07 09:41:02 -0400

    Some code refactor

--- find1m1th_prime.cpp
+++ find1m1th_prime.cpp
@@ -4,19 +4,21 @@
 #include <limits>
 #include <iostream>
 
-boost::dynamic_bitset<> sieve_of_sundaram(long N )
+boost::dynamic_bitset<> sieve_of_sundaram(long N)
 {
     const long M = N / 2 ;
-    boost::dynamic_bitset<> sieve(M) ;
-    sieve.flip() ;
+    boost::dynamic_bitset<> sieve(M);
+    sieve.flip(); //set all the bits to true
 
-    for( long i = 1 ; i < M ; ++i )
+    for( long i = 1; i < M; ++i)
     {
-        const long L = (M-i) / ( 2*i + 1 ) ;
-        for( long j = i ; j <= L ; ++j ) sieve[ i + j + 2*i*j ] = false ;
+        const long L = (M-i)/(2*i + 1);
+        for( long j = i ; j <= L ; ++j) {
+            sieve[i + j + 2*i*j] = false;
+        }
     }
 
-    return sieve ;
+    return sieve;
 }
 
 int nth_prime_number( long n )
@@ -24,11 +26,16 @@ int nth_prime_number( long n )
     int number = 2 ;
     if( n > 1 )
     {
-        double ubound = std::max( 100.0, n * std::log(n) * 1.5 ) ;
-        auto sieve = sieve_of_sundaram(ubound) ;
+        double ubound = n * std::log(n) * 1.5;
+        auto sieve = sieve_of_sundaram(ubound);
         long pos = 1 ;
-        for( long i = pos ; n > 1 ; ++i ) if( sieve[i] ) { pos = i ; --n ; }
-        number = pos * 2 + 1 ;
+        for( long i = 1; n > 1 ; ++i) {
+            if( sieve[i] ) {
+                pos = i ; 
+                --n;
+            }
+        }
+        number = pos * 2 + 1;
     }
 
     return number ;
